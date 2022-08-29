f648831ccf982abeca1cad964eff3213a98c0f18
HiromasaHori <horihiromasa.tech@gmail.com>
2022-08-30 02:11:50 +0900

    CocoaPods完了

--- CocoaPodsApp/ViewController.swift
+++ CocoaPodsApp/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import PKHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        HUD.flash(.success, delay: 2.0)
     }
 
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'CocoaPodsApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CocoaPodsApp
+  pod 'PKHUD','~>5.0'
+
+end
